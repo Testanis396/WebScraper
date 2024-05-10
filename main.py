2fb1ee80fedcb03973e2afd33c5253561a0f36ee
Tomas Estanislao <tj.estan@gmail.com>
2024-05-10 15:21:46 -0500

    wrapped in loop to process list and iterate through pages. Temp 'breaks'

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.royalroad.com/fictions/search?page=1&advanced=true"
+pageNumber = 1
+lastNumber = 0
+while True:
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    # Process
+    tempList = doc.find_all("div", class_="row fiction-list-item")
+    for item in tempList:
+        print(item)
+        break
+    break
+    # Next button
+    if (pageNumber == 1):
+        pagUl = doc.find("ul", class_="pagination")
+        lastBtn = pagUl.find("a", string = "Last »")
+        lastNumber = lastBtn.get("data-page")
+    elif (pageNumber == lastNumber): break
+    pageNumber += 1
+    url = "https://www.royalroad.com/fictions/search?page={}&advanced=true".format(pageNumber)
 
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-##print(doc.prettify())
-##<a alt="" class="col-md-1 hidden-xs hidden-sm fiction-detail" data-fid="75345">
-print(doc.find_all("div", class_="row fiction-list-item"))
