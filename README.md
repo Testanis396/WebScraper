8803adab27619173404d8081e23386251fee5c9d
Tomas Estanislao <tj.estan@gmail.com>
2024-05-10 01:36:53 -0500

    updated README and identified each title element

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WebScraper
+
+* Scraping RoyalRoad with python's BeautifulSoup framework. Goal is to compile a dataset in a CSV format, manipulate, and display in the browser. Could use pandas to visualize the data: filter and query. Possibility to implement accounts to allow users to create custom lists.
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.royalroad.com/fictions/search?page=1&advanced=true"
+
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+##print(doc.prettify())
+##<a alt="" class="col-md-1 hidden-xs hidden-sm fiction-detail" data-fid="75345">
+print(doc.find_all("div", class_="row fiction-list-item"))
